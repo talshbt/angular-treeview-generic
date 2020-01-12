987250864734231eb81f54969a78b61f60bbd995
talshbt <talshbt@gmail.com>
2020-01-12 08:56:44 +0200

    3 files modified

--- app.component.ts
+++ app.component.ts
@@ -8,24 +8,24 @@ import { TreeViewComponent} from '@syncfusion/ej2-angular-navigations';
     templateUrl: 'app.component.html'
 })
 export class AppComponent {
-    @ViewChild ('tree') tree: TreeViewComponent;
-public data = [
-  {NodeId: 1, NodeText: 'Music', Nodechild: [{NodeId: '01-01', NodeText: 'Gouttes.mp3'}]},
-     {NodeId: '02', NodeText: 'Videos',
-            Nodechild: [
-                {NodeId: '02-01', NodeText: 'Naturals.mp4'},
-                {NodeId: '02-02', NodeText: 'Wild.mpeg'}
-        ]
-        },
-]
-   public field:Object ={ dataSource: this.data, id: 'NodeId', text: 'NodeText', child: 'Nodechild' };
 
-   public onCreate(){  
-     var proxy = this;
-     document.getElementById('btn').addEventListener('click', function(){ debugger
-          proxy.tree.fields.dataSource= window.treeData;
-          document.getElementById('text').innerHTML = 'TreeView with XML data'
-     })
+
+
+    @ViewChild ('tree',{static:false}) tree: TreeViewComponent;
+
+
+
+    
+
+
+   public field:Object ={ dataSource: window.treeData, id: 'NodeId', text: 'NodeText', child: 'Nodechild' };
+
+  //  public onCreate(){  
+  //    var proxy = this;
+  //    document.getElementById('btn').addEventListener('click', function(){ debugger
+  //         proxy.tree.fields.dataSource= window.treeData;
+  //         document.getElementById('text').innerHTML = 'TreeView with XML data'
+  //    })
      
-   }
+  //  }
 }
